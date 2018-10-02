99b08a5d5e57e9445a2a5dcbf2f8af88ad8d5d80
hvd <i.hvd96@gmail.com>
2018-10-02 10:14:36 +0700

    init admin layout and manager user

---
+++ app/controllers/recent_views_controller.rb
@@ -0,0 +1,5 @@
+class RecentViewsController < ApplicationController
+  before_action :load_recently, only: :recently
+  
+  def recently; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,14 @@ Rails.application.routes.draw do
   post "/product_rating/:id", to: "products#rating", as: "rating"
   get "/suggestions", to: "suggestions#new"
   post "/suggestions", to: "suggestions#create"
+  get "/recently", to: "recent_views#recently"
 
   resources :categories
   resources :products
   resources :users
+
+  namespace :admin do
+    root "static_pages#home"
+    resources :users
+  end
 end
