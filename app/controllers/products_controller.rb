d5f35d3d4095c29cb957c51bddb1f97ac0e8ebca
hvd <i.hvd96@gmail.com>
2018-10-02 10:14:36 +0700

    first gui

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  before_action :load_product, only: [:show]
+
+  def show; end
+
+  private
+  def load_product
+    @product = Product.find_by(id: params[:id])
+  end
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @hot = Product.all.take(12)
+  end
 end
